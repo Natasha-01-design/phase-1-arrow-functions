e824f341bb8c73dba6d6e6b346356b8f0c7a9a4e
Natasha-01-design <natashakarwitha6@gmail.com>
2025-06-16 00:05:46 +0300

    m

---
+++ index.js
@@ -0,0 +1,12 @@
+const saturdayFun = (role= "roller-skate") => `This Saturday, I want to ${role}!`;
+const mondayWork = (office = "go to the office") => `This Monday, I will ${office}.`;
+const wrapAdjective = (visualFlair = "*") => {
+  return (adjective = "special") => {
+    return `You are ${visualFlair}${adjective}${visualFlair}!`;
+  };
+};
+const divide = function() {
+  return 2000 / 100;
+};
+const square = n => n * n;
+const add = (c, d) => c + d;
